8903b0c90a487da273e3007ae7ebe30a5177f002
knlao <1166286-7@g.puiching.edu.mo>
2023-10-09 07:35:45 +0800

    changed shorycut from choices to blanks

--- pages/shortcut.js
+++ pages/shortcut.js
@@ -6,7 +6,7 @@ function About() {
             <p className="text-xl font-bold">Shortcut</p>
             <p>Links for recent tests and exams</p>
             <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/chem/chemical-solubility/chemical/solubility/choices">2023高一第一段第一次化學測驗溶解性表</Link>
+            <Link className="text-l font-semibold text-blue-400 underline" href="/chem/chemical-solubility/chemical/solubility/blanks">2023高一第一段第一次化學測驗溶解性表</Link>
         </div>
     );
 }
