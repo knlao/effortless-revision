b03eb2adc034cbac231eebbb6ec1b04f23176451
knlao <85008961+knlao@users.noreply.github.com>
2023-10-26 17:09:14 +0800

    Update shortcut.js

--- pages/shortcut.js
+++ pages/shortcut.js
@@ -11,6 +11,10 @@ function About() {
             <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-test2-quotes/first/whole/blanks">2023高一第一段第二次中文測驗名句</Link>
             <br></br>
             <Link className="text-l font-semibold text-blue-400 underline" href="/bio/s4-term1-bigtest-words/chi/eng/blanks">2023高一第一段生物大測英文生字</Link>
+            <br></br>
+            <p>Links for recent tests and exams</p>
+            <br></br>
+            <Link className="text-l font-semibold text-blue-400 underline" href="/chem/chemical-solubility/chemical/solubility/blanks">化學溶解性表</Link>
         </div>
     );
 }
