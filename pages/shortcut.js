cff9aca9b7930d703068c671c51e06e184ac0c70
knlao <85008961+knlao@users.noreply.github.com>
2023-10-26 16:59:41 +0800

    Update bio test to shortcut

--- pages/shortcut.js
+++ pages/shortcut.js
@@ -9,6 +9,8 @@ function About() {
             <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-test2-idioms/meaning/idiom/blanks">2023高一第一段第二次中文測驗熟語</Link>
             <br></br>
             <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-test2-quotes/first/whole/blanks">2023高一第一段第二次中文測驗名句</Link>
+            <br></br>
+            <Link className="text-l font-semibold text-blue-400 underline" href="/bio/s4-term1-bigtest-words/chi/eng/blanks">2023高一第一段生物大測英文生字</Link>
         </div>
     );
 }
