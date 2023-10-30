b14fba1cbbe5745a8f6518fd4294ea0d9e1a2d3b
knlao <1166286-7@g.puiching.edu.mo>
2023-10-30 15:09:34 +0800

    remove redundant stuff

--- pages/[subject]/[unit]/[a]/[b]/[mode].js
+++ pages/[subject]/[unit]/[a]/[b]/[mode].js
@@ -28,14 +28,6 @@ function App() {
     content = <div>not found</div>
   }
 
-  /*
-    if (mode === "choices") {
-    content = <MCQ words={data} a={a} b={b}></MCQ>;
-  } else if (mode === "blanks") {
-    content = <FITB words={data} a={a} b={b}></FITB>
-  }
-  */
-
   return (
     <div>
         { error && <div>{ error }</div> }
