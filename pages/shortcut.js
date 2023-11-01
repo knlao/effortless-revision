26dbf06ddcba50549946e3308a4f78ee30535bb9
knlao <1166286-7@g.puiching.edu.mo>
2023-11-01 13:14:16 +0800

    Update shortcut.js

--- pages/shortcut.js
+++ pages/shortcut.js
@@ -10,11 +10,7 @@ function About() {
             <br></br>
             <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-test2-quotes/first/whole/blanks">2023高一第一段第二次中文測驗名句</Link>
             <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/bio/s4-term1-ch7-words/chi/eng/blanks">2023高一第一段生物Ch7英文生字</Link>
             <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/bio/s4-term1-ch8-words/chi/eng/blanks">2023高一第一段生物Ch8英文生字</Link>
-            <br></br>
-            <br></br> 
             <p>Maybe useful links</p>
             <br></br>
             <Link className="text-l font-semibold text-blue-400 underline" href="/chem/chemical-solubility/chemical/solubility/blanks">化學溶解性表</Link>
