6daf9302786830386edf29537b200b89d1a45dff
knlao <85008961+knlao@users.noreply.github.com>
2023-11-18 17:05:17 +0800

    Update shortcut.js

--- pages/shortcut.js
+++ pages/shortcut.js
@@ -7,19 +7,9 @@ function About() {
             <Head><title>Shortcut - Effortless Revision</title></Head>
             <p className="text-xl font-bold">Shortcut</p>
             <p>Maybe useful links for recent tests and exams</p>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/eng/s4-term1-exam-adv-aa-vocabs/chi/eng/blanks">高一第一段考試進階英文AthensAnthology生字</Link>
+            <Link className="text-l font-semibold text-blue-400 underline" href="/misc/fruits/a/b/blanks">misc/fruits</Link>
             <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/eng/s4-term1-exam-adv-life-vocabs/chi/eng/blanks">高一第一段考試進階英文Life生字</Link>
-            <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-exam-idioms/pinyin/idiom/blanks">高一第一段中文考試成語</Link>
-            <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-exam-proverbs/meaning/idiom/blanks">高一第一段中文考試熟語</Link>
-            <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/chi/s4-term1-exam-quotes/first/whole/blanks">高一第一段中文考試名句</Link>
-            <br></br>
-            <Link className="text-l font-semibold text-blue-400 underline" href="/chem/chemical-solubility/chemical/solubility/blanks">化學溶解性表</Link>
-            <br></br>
-            <a className="text-l font-semibold text-blue-400 underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">考試加油</a>
+            <a className="text-l font-semibold text-blue-400 underline" href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">yeet</a>
         </div>
     );
 }
